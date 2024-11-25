2bd7c780a5380300eeab66d7f453ca98235d8ea7
vipinrao009 <vipinrao2018@gmail.com>
2024-11-25 13:51:24 +0530

    Made responsive admin dashboard for mobile

--- src/components/Admin/AdminHeader.jsx
+++ src/components/Admin/AdminHeader.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import AdminSidebarForMobile from './AdminSidebarForMobile';
+import logo from '../../assets/Logo.PNG'
 
 const AdminHeader = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setOpenSidebar(!openSidebar);
+  };
+
   return (
-    <div className="bg-white shadow-md p-4 flex items-center justify-between">
-      <h1 className="text-2xl font-bold">Faculty Excellence Tracker</h1>
-      <div className="flex space-x-4">
+    <div className="bg-white shadow-md p-3 sm:p-4 flex items-center justify-between fixed top-0 left-0 w-full z-50 sm:static">
+      <img className='h-9 w-10 sm:hidden' src={logo} alt="" />
+      <h1 className="text-2xl font-bold hidden sm:text-2xl sm:flex">Faculty Excellence Tracker</h1>
+      <div className=" hidden sm:flex space-x-1">
         <div>
           <select
             // value={selectedLanguage}
@@ -17,6 +27,22 @@ const AdminHeader = () => {
           </select>
         </div>
         <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Logout</button>
+      </div>
+      <FaBars onClick={toggleSidebar} className='text-2xl sm:hidden' />
+
+      {openSidebar && (
+        <div 
+         className='fixed inset-0 bg-gray-800 bg-opacity-50 z-40 sm:hidden'
+         onClick={toggleSidebar}>
+        </div>
+      )}
+
+      <div
+        className={`fixed top-0 left-0  bg-blue-500 text-white shadow-lg z-50 transform ${
+          openSidebar ? 'translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300 sm:hidden`}
+      >
+        <AdminSidebarForMobile />
       </div>
     </div>
   );
--- src/components/Admin/AdminSidebarForMobile.jsx
+++ src/components/Admin/AdminSidebarForMobile.jsx
@@ -4,8 +4,8 @@ import { HomeIcon, DocumentSearchIcon, UserIcon } from '@heroicons/react/outline
 
 const AdminSidebar = () => {
   return (
-    <div className="h-auto hidden   bg-gray-800 text-white w-64 sm:flex flex-col">
-      <div className="text-2xl font-bold p-6 border-b border-gray-700">
+    <div className="h-full bg-gray-800 text-white w-60 sm:flex flex-col">
+      <div className="text-xl font-bold p-6 border-b border-gray-700">
         Admin Dashboard
       </div>
       <nav className="flex-1 px-4 py-6 space-y-4">
@@ -17,10 +17,26 @@ const AdminSidebar = () => {
           <UserIcon className="w-5 h-5 mr-3" />
           Appraisals
         </Link>
-        <Link to="/admin-dashboard/faculty-stats" className="flex items-center p-2 hover:bg-gray-700 rounded">
+        <Link to="/admin-dashboard/faculty-stats" className="flex mb-6 items-center p-2 hover:bg-gray-700 rounded">
           <DocumentSearchIcon className="w-5 h-5 mr-3" />
           Faculty Stats
         </Link>
+
+        <div className='ml-3'>
+          <select
+            // value={selectedLanguage}
+            // onChange={handleLanguageChange}
+            className="bg-blue-500 text-white py-1 px-2 rounded-md"
+          >
+            <option value="English">English</option>
+            <option value="Spanish">Hindi</option>
+            <option value="French">Tamil</option>
+          </select>
+        </div>
+        
+        <button className='px-4 py-1 w-full bg-red-500 hover:bg-red-400 rounded-md'>
+            Logout
+        </button>
 
         {/* Add more navigation links as required */}
       </nav>
